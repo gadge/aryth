29fc08c5c2ab6d80591d606eed296247f8e52fae
alkashi <hoyeungw@outlook.com>
2020-05-20 09:46:54 +0800

    minor fix

--- packages/rank-vector/src/duoRank.js
+++ packages/rank-vector/src/duoRank.js
@@ -7,35 +7,27 @@ const isAlphabetic = x => /[A-Za-z0-9]+/.test(x)
 /**
  *
  * @param words
- * @param filter
- * @param comparer
- * @param restFilter
- * @param restComparer
+ * @param {Object} x
+ * @param {Object} y
  * @return {number[]}
  */
 export const duoRank = (
   words,
-  {
-    filter = isNumeric,
-    comparer = NUM_DESC,
-  } = {},
-  {
-    filter: restFilter = isAlphabetic,
-    comparer: restComparer = STR_DESC
-  } = {}
+  x = { filter: isNumeric, comparer: NUM_DESC, },
+  y = { filter: isAlphabetic, comparer: STR_DESC }
 ) => {
   const primVec = [], restVec = []
-  iterate(words, x => {
-    if (filter(x)) return void primVec.push(x)
-    if (restFilter(x)) return void restVec.push(x)
+  iterate(words, v => {
+    if (x.filter(v)) return void primVec.push(v)
+    if (y.filter(v)) return void restVec.push(v)
   })
   const
-    primSorted = primVec.sort(comparer),
-    restSorted = restVec.sort(restComparer)
+    primSorted = primVec.sort(x.comparer),
+    restSorted = restVec.sort(y.comparer)
   return mapper(words, x => {
     let i
-    if ((i = primSorted.indexOf(x)) >= 0) { return -(i + 1) }
-    if ((i = restSorted.indexOf(x)) >= 0) { return i + 1 }
+    if ((i = primSorted.indexOf(x)) >= 0) { return (i + 1) }
+    if ((i = restSorted.indexOf(x)) >= 0) { return -(i + 1) }
     return NaN
   })
 }
--- packages/rank-vector/test/duorank.test.js
+++ packages/rank-vector/test/duorank.test.js
@@ -1,19 +1,29 @@
-import { NUM_DESC, STR_ASC } from '@aryth/comparer'
-import { SimpleVectors }     from '@foba/foo'
+import { NUM_ASC, NUM_DESC, STR_ASC } from '@aryth/comparer'
+import { SimpleVectors }              from '@foba/foo'
 import { says }              from '@palett/says'
 import { DecoMatrix }        from '@spare/logger'
 import { STR }               from '@typen/enum-data-types'
 import { isNumeric }         from '@typen/num-loose'
+import { duoRank }           from '../src/duoRank'
 import { rank }              from '../src/rank'
+
+const VectorCollection = SimpleVectors
+VectorCollection.combined = ['foo', 'bar', 'zen', '-127', 0, 127]
 
 const test = () => {
   for (const [key, vec] of Object.entries(SimpleVectors)) {
     const strRanks = rank(vec, STR_ASC, x => typeof x === STR)
-    const numRanks = rank(vec, NUM_DESC, isNumeric)
+    const numRanks = rank(vec, NUM_ASC, isNumeric)
+    const duoRanks = duoRank(
+      vec,
+      { filter: isNumeric, comparer: NUM_ASC },
+      { filter: x => typeof x === STR, comparer: STR_ASC }
+    )
     const matrix = [
       vec,
       strRanks,
       numRanks,
+      duoRanks,
     ]
     matrix |> DecoMatrix({ delim: ' | ' }) |> says[key]
   }
