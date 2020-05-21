2e070458aa65b4e92c8d49ee262b64680a04bc9e
alkashi <hoyeungw@outlook.com>
2020-05-21 10:20:18 +0800

    minor fix

--- packages/rank-vector/test/duorank.test.js
+++ packages/rank-vector/test/duorank.test.js
@@ -1,28 +1,30 @@
-import { NUM_ASC, NUM_DESC, STR_ASC } from '@aryth/comparer'
-import { SimpleVectors }              from '@foba/foo'
-import { says }              from '@palett/says'
-import { DecoMatrix }        from '@spare/logger'
-import { STR }               from '@typen/enum-data-types'
-import { isNumeric } from '@typen/num-loose'
-import { duorank }   from '../src/duoRank'
-import { rank }      from '../src/rank'
+import { NUM_ASC, STR_ASC }    from '@aryth/comparer'
+import { SimpleVectors }       from '@foba/foo'
+import { says }                from '@palett/says'
+import { DecoMatrix }          from '@spare/logger'
+import { isLiteral, isString } from '@typen/literal'
+import { isNumeric }           from '@typen/num-loose'
+import { duorank }             from '../src/duorank'
+import { rank }                from '../src/rank'
 
 const VectorCollection = SimpleVectors
 VectorCollection.combined = ['foo', 'bar', 'zen', '-127', 0, 127]
+VectorCollection.countries = ['GBR', 'KOR', 'JPN', 'IND', 'DEU', 'CHN', 'USA']
+VectorCollection.cities = ['Erbil', 'Abuja', 'Cochabamba', 'Cochin', 'Stockton,Ranchi', 'Kathmandu', 'Madras', 'Ndjamena']
 
 const test = () => {
   for (const [key, vec] of Object.entries(SimpleVectors)) {
-    const strRanks = rank(vec, STR_ASC, x => typeof x === STR)
+    const strRanks = rank(vec, STR_ASC, isString)
     const numRanks = rank(vec, NUM_ASC, isNumeric)
     const duoRanks = duorank(
       vec,
       { filter: isNumeric, comparer: NUM_ASC },
-      { filter: x => typeof x === STR, comparer: STR_ASC }
+      { filter: isLiteral, comparer: STR_ASC }
     )
     const matrix = [
       vec,
-      strRanks,
-      numRanks,
+      // strRanks,
+      // numRanks,
       duoRanks,
     ]
     matrix |> DecoMatrix({ delim: ' | ' }) |> says[key]
