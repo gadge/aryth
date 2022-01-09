79552477767c9667027d86e42df34139db5fdf9b
hazen <hoyeungw@outlook.com>
2022-01-09 16:00:38 +0800

    export lately added functions

--- packages/math/index.js
+++ packages/math/index.js
@@ -4,10 +4,12 @@ export {
   E7, E8, E9,
   E10, E11, E12,
   E13, E14, E15,
-}                                                    from './assets/large'
-export { abbr }                                      from './src/abbr'
-export { abs, intAbs }                               from './src/abs'
-export { almostEquals, almostInt }                   from './src/almost'
-export { constraint }                                from './src/constraint'
-export { intExpon }                                  from './src/expon'
-export { round, roundD1, roundD2, roundD3, roundD4 } from './src/round'
+}                                                             from './assets/large'
+export { abbr }                                               from './src/abbr'
+export { abs, intAbs }                                        from './src/abs'
+export { almostEquals, almostInt }                            from './src/almost'
+export { constraint }                                         from './src/constraint'
+export { intExpon }                                           from './src/expon'
+export { round, roundD1, roundD2, roundD3, roundD4 }          from './src/round'
+export { limit, restrict, limitAboveZero, restrictAboveZero } from './src/constraint'
+export { has, hasOpen, hasROpen, hasLOpen }                   from './src/has'
