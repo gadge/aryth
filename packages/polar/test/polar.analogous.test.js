609e3a9696e6e481db4fbb3a898aec2019584cfb
hazen <hoyeungw@outlook.com>
2022-01-15 17:57:50 +0800

    add test

---
+++ packages/polar/test/polar.analogous.test.js
@@ -0,0 +1,8 @@
+import { deco, says } from '@spare/logger'
+import { Polar }      from '../src/Polar'
+
+const polar = Polar.build(100, 120)
+
+polar |> deco |> says['polar']
+
+polar.analogous(5, 5)  |> deco |> says['polar'].br('analogous')
