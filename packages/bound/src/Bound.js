0ec62aa8aab1b44931f8013b9fbbd17be4ad09af
hazen <hoyeungw@outlook.com>
2022-01-16 11:41:48 +0800

    add property dif

--- packages/bound/src/Bound.js
+++ packages/bound/src/Bound.js
@@ -7,8 +7,22 @@ export class Bound {
   }
   static build(min, max) { return new Bound(min, max) }
 
+  get dif() { return this.max - this.min }
   has(num) { return this.min <= num && num <= this.max }
   hasLOpen(num) { return this.min < num && num <= this.max }
   hasROpen(num) { return this.min <= num && num < this.max }
   hasOpen(num) { return this.min < num && num < this.max }
+
+  limit(value) {
+    if (value < this.min) return this.min
+    if (value > this.max) return this.max
+    return value
+  }
+
+  restrict(value) {
+    const delta = this.max - this.min
+    while (value < this.min) value += delta
+    while (value > this.max) value -= delta
+    return value
+  }
 }
