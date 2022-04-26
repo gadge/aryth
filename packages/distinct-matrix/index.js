3b12190ab6d04f944ca653c4ac1f5bf1510705d8
hazen <hoyeungw@outlook.com>
2022-04-26 14:51:34 +0800

    add distinctByColumn

--- packages/distinct-matrix/index.js
+++ packages/distinct-matrix/index.js
@@ -1,1 +1,12 @@
 export { distinct, distinctCount } from './src'
+
+export function distinctByColumn(mx, y) {
+  let [ h, w ] = size(mx)
+  if (!h || !w || y >= w) return [ [] ]
+  let targetMx = []
+  for (let i = 0, sourceRow; i < h; i++) {
+    sourceRow = mx[i]
+    if (targetMx.findIndex(targetRow => targetRow[y] === sourceRow[y]) < 0) targetMx.push(sourceRow)
+  }
+  return targetMx
+}
