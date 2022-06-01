9640bf30686f95dfc0a5f6627ecbfa5d4b7335cb
hoyeungw <hoyeungw@outlook.com>
2022-06-01 09:19:44 +0800

    change api of @aryth/math

--- packages/math/index.js
+++ packages/math/index.js
@@ -4,12 +4,16 @@ export {
   E7, E8, E9,
   E10, E11, E12,
   E13, E14, E15,
-}                                                             from './assets/large'
-export { abbr }                                               from './src/abbr'
-export { abs, intAbs }                                        from './src/abs'
-export { almostEqual, almostEquals, almostInt }               from './src/almost'
-export { constraint }                                         from './src/constraint'
-export { intExpon }                                           from './src/expon'
-export { round, roundD1, roundD2, roundD3, roundD4 }          from './src/round'
-export { limit, restrict, limitAboveZero, restrictAboveZero } from './src/constraint'
-export { has, hasOpen, hasROpen, hasLOpen }                   from './src/has'
+}                                                    from './assets/large'
+export { abbr }                                      from './src/abbr'
+export { abs, intAbs }                               from './src/abs'
+export { almostEqual, almostEquals, almostInt }      from './src/almost'
+export { intExpon }                                  from './src/expon'
+export { round, roundD1, roundD2, roundD3, roundD4 } from './src/round'
+export { limBy, lim, recLim, lim0up, rec0up }        from './src/constraint'
+
+export { has, hasOpen, hasROpen, hasLOpen } from './src/has'
+
+export {
+  limBy as constraint, lim as limit, recLim as restrict, lim0up as limitAboveZero, rec0up as restrictAboveZero
+} from './src/constraint'
--- packages/math/src/constraint.js
+++ packages/math/src/constraint.js
@@ -1,24 +1,24 @@
-export const constraint = (x, min, max) => x > max ? max : x < min ? min : x
+export const limBy = (x, min, max) => x > max ? max : x < min ? min : x
 
-export const limit = ({ min, max }, value) => {
+export const lim = ({ min, max }, value) => {
   if (value < min) return min
   if (value > max) return max
   return value
 }
 
-export const restrict = ({ min, max }, value) => {
+export const recLim = ({ min, max }, value) => {
   const delta = max - min
   while (value < min) value += delta
   while (value > max) value -= delta
   return value
 }
 
-export const limitAboveZero = (value, max) => {
+export const lim0up = (value, max) => {
   if (value < 0) return 0
   if (value > max) return max
   return value
 }
-export const restrictAboveZero = (value, max) => {
+export const rec0up = (value, max) => {
   while (value < 0) value += max
   while (value > max) value -= max
   return value
